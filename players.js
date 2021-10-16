b7eff1790e173af4857ba92eed61eef0d4b8444a
elandi <56363232+Elandi-rj@users.noreply.github.com>
2021-10-16 15:53:22 +1100

    Update players.js

--- players.js
+++ players.js
@@ -3,112 +3,7 @@ players = [
         aliases: ['steve', 'arvinge'],
         id: 11020,
         channel: 'arvinge',
-    },
-    {
-        aliases: ['elandi'],
-        id: 170674,
-        channel: 'elandi',
-    },
-    {
-        aliases: ['error', 'ewwow'],
-        id: 204,
-        channel: 'aorer',
-    },
-    {
-        aliases: ['matty'],
-        id: 1320,
-        channel: 'au_matty',
-    },
-    {
-        aliases: ['megawop'],
-        id: 630,
-    },
-    {
-        aliases: ['skinny'],
-        id: 78463,
-        channel: 'skinny_lad',
-    },
-    {
-        aliases: ['boshy'],
-        id: 39902,
-        channel: 'boshytf',
-    },
-    {
-        aliases: ['kater', 'exor'],
-        id: 99892,
-    },
-    {
-        aliases: ['newjuls'],
-        id: 281915,
-        channel: 'newjuls',
-    },
-    {
-        aliases: ['fences'],
-        id: 1701,
-    },
-    {
-        aliases: ['riot'],
-        id: 145,
-        channel: 'riotbz',
-    },
-    {
-        aliases: ['tripbwai', 'trip', 'bwai'],
-        id: 11255,
-        channel: 'tripbwai',
-    },
-    {
-        aliases: ['kidder'],
-        id: 76057,
-    },
-    {
-        aliases: ['un gato', 'ungatoo', 'ungato', 'gato'],
-        id: 82781,
-    },
-    {
-        aliases: ['teardrop'],
-        id: 3677,
-        channel: 'teardrop_tf',
-    },
-    {
-        aliases: ['kaptain'],
-        id: 68414,
-        channel: 'kaptain__',
-    },
-    {
-        aliases: ['superchuck'],
-        id: 344,
-        channel: 'superchuck_',
-    },
-    {
-        aliases: ['vice'],
-        id: 10736,
-    },
-    {
-        aliases: ['zike'],
-        id: 5164,
-        channel: 'zike1017',
-    },
-    {
-        aliases: ['soup'],
-        id: 2220,
-        channel: 'souptt',
-    },
-    {
-        aliases: ['starkie'],
-        id: 72697,
-    },
-    {
-        aliases: ['deceptive'],
-        id: 196930,
-    },
-    {
-        aliases: ['wild'],
-        id: 176220,
-    },
-    {
-        aliases: ['juup'],
-        id: 363721,
-    },
+    }
 ]
 
 function FindTempusRecordPlayer(Id) {
@@ -130,6 +25,16 @@ function FindPlayerFromChannel(channelName) {
         '#' + p.channel.toLowerCase() == channelName.toLowerCase()
     );
 }
+function GetChannels() {
+    let channels = [];
+    players.forEach(p => {
+        if (p.channel) {
+            channels.push(p.channel)
+        }
+    })
+    return channels;
+}
+exports.GetChannels = GetChannels;
 exports.FindTempusRecordPlayer = FindTempusRecordPlayer;
 exports.FindPlayer = FindPlayer;
 exports.FindPlayerFromChannel = FindPlayerFromChannel;
